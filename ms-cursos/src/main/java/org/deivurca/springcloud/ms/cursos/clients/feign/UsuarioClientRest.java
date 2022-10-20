7d857af26e24623ab899077498c1f0330da044f5
dhurrego <deivid_199615@hotmail.com>
2022-10-19 20:17:33 -0500

    Actualizando la aplicacion, usando spring-cloud-kubernetes

--- ms-cursos/src/main/java/org/deivurca/springcloud/ms/cursos/clients/feign/UsuarioClientRest.java
+++ ms-cursos/src/main/java/org/deivurca/springcloud/ms/cursos/clients/feign/UsuarioClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "ms-usuarios", url = "${ms-usuarios.url}")
+@FeignClient(name = "ms-usuarios")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
--- ms-usuarios/src/main/java/org/deivurca/springcloud/ms/usuarios/clients/feign/CursoClientFeign.java
+++ ms-usuarios/src/main/java/org/deivurca/springcloud/ms/usuarios/clients/feign/CursoClientFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-cursos", url = "${ms-cursos.url}")
+@FeignClient(name = "ms-cursos")
 public interface CursoClientFeign {
 
     @DeleteMapping("/desasignar-usuario/{usuarioId}")
