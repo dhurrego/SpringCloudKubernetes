4e3dc5e18dfdf760af5b5587fc89bf783c8fa123
dhurrego <deivid_199615@hotmail.com>
2022-10-30 23:52:24 -0500

    Creacion de nuevo microservicio API Gateway

---
+++ ms-gateway/src/main/java/org/deivurca/springcloud/ms/gateway/MsGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.deivurca.springcloud.ms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayApplication.class, args);
+	}
+
+}
