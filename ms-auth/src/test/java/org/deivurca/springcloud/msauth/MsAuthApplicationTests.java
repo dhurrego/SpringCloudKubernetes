501c0b438bd61fe70fc9b1f9e4bb66c650ffb3e7
dhurrego <deivid_199615@hotmail.com>
2022-11-14 21:12:42 -0500

    Creacion microservicio de autenticación ms-auth

---
+++ ms-auth/src/test/java/org/deivurca/springcloud/msauth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deivurca.springcloud.msauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
